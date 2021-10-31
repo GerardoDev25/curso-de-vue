39bf4cbfb14e643ad1a1f5178156212c29309c17
Gerardo Miranda <gerardodev25@gmail.com>
2021-10-31 18:58:48 -0400

    add pwa to 05_gradient_generator

--- 05_degradados/js/main.js
+++ 05_degradados/js/main.js
@@ -1,21 +1,20 @@
 const app = Vue.createApp({
   data: () => ({
     title: "Gradient Generator",
-    fcolor: "#48ef12",
-    scolor: "#eeeeee",
+    fcolor: "#d5ee11",
+    scolor: "#f59e24",
     orientation: 1,
   }),
   computed: {
     setColor() {
-      switch (Number(this.orientation)) {
-        case 1:
-          return `background: linear-gradient(to right,${this.fcolor}, ${this.scolor} );`;
-        case 2:
-          return `background: linear-gradient(to left,${this.fcolor}, ${this.scolor} );`;
-        case 3:
-          return `background: linear-gradient(to top,${this.fcolor}, ${this.scolor} );`;
-        case 4:
-          return `background: linear-gradient(to bottom,${this.fcolor}, ${this.scolor} );`;
+      if (this.orientation == 1) {
+        return `background: linear-gradient(to right, ${this.fcolor}, ${this.scolor} );`;
+      } else if (this.orientation == 2) {
+        return `background: linear-gradient(to left, ${this.fcolor}, ${this.scolor} );`;
+      } else if (this.orientation == 3) {
+        return `background: linear-gradient(to top, ${this.fcolor}, ${this.scolor} );`;
+      } else {
+        return `background: linear-gradient(to bottom, ${this.fcolor}, ${this.scolor} );`;
       }
     },
   },
