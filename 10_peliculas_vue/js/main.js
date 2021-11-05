163f747798d46ce8eb78daf6061fdbd082502236
Gerardo Miranda <gerardodev25@gmail.com>
2021-11-04 21:08:05 -0400

    sasdasdasdas

---
+++ 10_peliculas_vue/js/main.js
@@ -0,0 +1,27 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      title: "Movies Vue",
+      movieData: {},
+      movieTitle: "Spider Man",
+    };
+  },
+  mounted() {
+    this.getMovie();
+  },
+  methods: {
+    async getMovie() {
+      const search = this.movieTitle
+        .toLowerCase()
+        .replace(' ', "+");
+
+      const data = await fetch(
+        `https://www.omdbapi.com/?apikey=32bbcb34&t=${search}`
+      );
+
+      const jsonData = await data.json();
+
+      this.movieData = jsonData;
+    },
+  },
+});
